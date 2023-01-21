2f66c55d0811e4c56c09133bbfe47be0481394ab
Nicolas Choquet <nchoquet@norsys.fr>
2023-01-21 04:18:12 +0100

    Création d'un dashboard

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import LoginBox from "../components/loginBox";
 
 const Login = () => {
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    navigate('/dashboard');
+  })
+
   return (
     <LoginBox/>
   );
